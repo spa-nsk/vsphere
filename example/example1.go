3ca13f1af02ad4c42431078f29403880878ed0ea
spa-nsk <pavel_semenov@mail.ru>
2021-01-10 09:59:10 +0700

    change example1.go

--- example/example1.go
+++ example/example1.go
@@ -7,13 +7,14 @@ import (
 )
 
 func main() {
-	infra := vsphere.GlobalInitVMInfrastructure()        //инициализируем инвраструктуру и получаем указатель на нее
-	username := "login-name@domain"                      //имя учетной записи для авторизации на управляющем vcenter
-	password := "password"                               //пароль для учетной записи
-	infra.AddVCenter("vc-01.domain", username, password) //регистрируем управляющий центр vc-01.domain
-	infra.AddVCenter("vc-02.domain", username, password) //регистрируем управляющий центр vc-02.domain
-	infra.AddVCenter("vc-03.domain", username, password) //регистрируем управляющий центр vc-03.domain
-	for ; ; time.Sleep(20 * time.Second) {               //в бесконечном цикле опрашиваем метрики и выводим пример на экран
+	infra := vsphere.GlobalInitVMInfrastructure()                       //инициализируем инвраструктуру и получаем указатель на нее
+	username := "login-name@domain"                                     //имя учетной записи для авторизации на управляющем vcenter
+	password := "password"                                              //пароль для учетной записи
+	infra.AddVCenter("vc-01.domain", username, password)                //регистрируем управляющий центр vc-01.domain
+	infra.AddVCenter("vc-02.domain", username, password)                //регистрируем управляющий центр vc-02.domain
+	infra.AddVCenter("vc-03.domain", username, password)                //регистрируем управляющий центр vc-03.domain
+	infra.AddVCenter("vc-04.domain", "demo@new-domain", "new-password") //регистрируем управляющий центр vc-04.domain
+	for ; ; time.Sleep(20 * time.Second) {                              //в бесконечном цикле опрашиваем метрики и выводим пример на экран
 		infra.LoadHostAndVMMetric() // опрашиваем каждые 20 секунд
 		infra.ListVMPerf()          //выводим список метрик
 	}
